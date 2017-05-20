3c5caf25a2e8b9aefe9ce655312d71dd2da4bed2
no-bark <ja.hibbs@yahoo.com>
2017-05-20 02:26:18 -0700

    Started building AI logic, trying to do it right...

---
+++ BigLostDoggo/Assets/Team_Doggo/Scripts/MoveToAttackPosition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToAttackPosition : BasicAIBehavior
+{
+    Targeted myTargeted;
+
+    public float validDistance = 7.0f;
+    public float timer = 0.0f;
+
+    public bool movingToAttack = false;
+    public float speed = 2.0f;
+
+    void Start()
+    {
+        myTargeted = GetComponent<Targeted>();
+    }
+
+    void Update()
+    {
+        timer = Mathf.Max(0, timer - Time.deltaTime);
+    }
+
+    public override bool NeedsUpdate()
+    {
+        if (myTargeted.tweenDist < validDistance && !movingToAttack)
+        {
+            timer += 2 * Time.deltaTime;
+        }
+        else
+        {
+            timer = 3;
+        }
+
+        if(timer > 4.0f)
+        {
+            timer = 0;
+            movingToAttack = true;
+            return true;
+        }
+        if(movingToAttack)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void AIUpdate()
+    {
+        transform.position = transform.position + (myTargeted.tweenVec) * (speed * (Mathf.Min(myTargeted.tweenDist, 2)));
+    }
+}
---
+++ BigLostDoggo/Assets/Team_Doggo/Scripts/Targeted.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeted : MonoBehaviour {
+    public GameObject target = null;
+
+    public Vector3 tweenVec = new Vector3();
+    public float tweenDist = 0;
+
+    public delegate void TargetChangedEvent();
+    public event TargetChangedEvent onTargetChanged;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+    void CalculateStats()
+    {
+        tweenVec = (target.transform.position - transform.position);
+        tweenDist = tweenVec.magnitude;
+        tweenVec = tweenVec.normalized;
+    }
+
+    public void TargetChange(GameObject newTarget)
+    {
+        CalculateStats();
+        onTargetChanged();
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        CalculateStats();
+	}
+}
